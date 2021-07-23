0562e5ad89383880391645dee64ae8829afbb874
Elias Akawako <Elikescoding@outlook.com>
2021-07-23 20:23:15 +0100

    added some javascript stuff

---
+++ MERN/Javascript/Javacheetsheet.js
@@ -0,0 +1,74 @@
+/* 
+
+Javascript types 
+-----------------------
+
+Number 
+String
+Boolean
+undefined
+null
+symbol
+object 
+
+
+Comparisons
+-------------------
+
+!==
+===
+>=
+<=
+>
+<
+
+Variables
+------------
+
+var
+let 
+const
+
+Conditionals 
+-------------
+
+if 
+else 
+esle if 
+terniary operator
+case switch
+
+Logic 
+------
+
+&&
+||
+!
+
+Functions
+----------
+
+var a = function fName( params here ) { code here  }
+function fName( ) { }
+return
+()=>{}
+
+Data Structures
+----------------
+
+Arrays 
+Objects
+
+Looping
+--------
+
+For
+while
+do
+for each
+
+
+
+
+
+*/
