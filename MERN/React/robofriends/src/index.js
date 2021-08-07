a9b126521d22fd9e64f4a49f09ed428ec2aeb5a7
Elias Akawako <Elikescoding@outlook.com>
2021-08-07 22:04:16 +0100

    added redux to the robotFriends app

--- MERN/React/robofriends/src/index.js
+++ MERN/React/robofriends/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware,combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots,requestRobots})
+const store = createStore(rootReducer,applyMiddleware(thunkMiddleware,logger))
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello 
-    greeting = {
-      'Hello' +
-      'React Ninja'
-    }/>
+    <Provider store={store}>
+    < App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
